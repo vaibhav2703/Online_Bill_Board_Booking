ee4246625ae90479b64d0d91537f1e7acbbc89b2
vaibhav2703 <vsnarwade7117@gmail.com>
2026-01-16 23:39:45 +0530

    Remove old Frontend folder, keeping only Frontend_2.0

--- Frontend/src/components/ui/label.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Label = ({ children, className = '', ...props }) => (
-  <label className={`block text-sm font-medium text-gray-700 ${className}`} {...props}>
-    {children}
-  </label>
-);
